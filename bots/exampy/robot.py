fdcc8425e7709c24f8ca2c606b43a78076227832
Kelvin Lu <kelvinlu@mit.edu>
2019-01-09 03:07:28 -0500

    crusaders sometimes nav correctly

--- bots/exampy/robot.py
+++ bots/exampy/robot.py
@@ -1,18 +1,20 @@
 from battlecode import BCAbstractRobot, SPECS
 import battlecode as bc
 import random
+import nav
 
 __pragma__('iconv')
 __pragma__('tconv')
 __pragma__('opov')
 
+
+
 # don't try to use global variables!!
 class MyRobot(BCAbstractRobot):
-    step = -1
+
+    destination = None
 
     def turn(self):
-        self.step += 1
-        self.log("START TURN " + self.step)
         if self.me['unit'] == SPECS['CRUSADER']:
             self.log("Crusader health: " + str(self.me['health']))
 
@@ -37,13 +39,15 @@ class MyRobot(BCAbstractRobot):
                 return self.attack(r['x'] - self.me['x'], r['y'] - self.me['y'])
 
             # The directions: North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest
-            choices = [(0,-1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
-            choice = random.choice(choices)
-            self.log('TRYING TO MOVE IN DIRECTION ' + str(choice))
-            return self.move(*choice)
+            my_coord = (self.me['x'], self.me['y'])
+            if not self.destination:
+                self.destination = nav.reflect(self.map, my_coord, self.me['id'] % 2)
+            movement = nav.calculate_dir(my_coord, self.destination, as_coord=True)
+            self.log('TRYING TO MOVE IN DIRECTION ' + str(self.destination))
+            return self.move(*movement)
 
         elif self.me['unit'] == SPECS['CASTLE']:
-            if self.step < 10:
+            if self.me['turn'] < 10:
                 self.log("Building a crusader at " + str(self.me['x']+1) + ", " + str(self.me['y']+1))
                 return self.build_unit(SPECS['CRUSADER'], 1, 1)
 
