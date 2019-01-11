b94a3f1edd5e111751710fec7c6bf59892be2449
Nate Foss <npfoss@gmail.com>
2019-01-11 01:42:59 -0500

    revert to working java bot

--- bots/example_java/MyRobot.java
+++ bots/example_java/MyRobot.java
@@ -1,47 +1,60 @@
 package bc19;
-import java.awt.Point;
 
 public class MyRobot extends BCAbstractRobot {
-	public int turn;
-	public Point destination;
 
     public Action turn() {
-    	turn++;
+        if (me.unit == SPECS.CASTLE) {
+            log("Building a pilgrim.");
+            return buildUnit(SPECS.PILGRIM,1,0);
+        }
+        return move(1,0);
 
-    	if (me.unit == SPECS.CASTLE) {
-    		if (turn == 1) {
-    			log("Building a pilgrim.");
-    			return buildUnit(SPECS.PILGRIM,1,0);
-    		}
-
-			Robot[] visibleRobots = getVisibleRobots();
-			for(Robot r: visibleRobots) {
-				if (r.team != me.team) {
-					int diffX = r.x - me.x;
-					int diffY = r.y - me.y;
-					return attack(diffX, diffY);
-				}
-			}
-
-			Point myLocation = new Point(me.x, me.y);
-
-			if (destination == null) {
-				destination = Navigation.reflect(myLocation, getPassableMap(), me.id % 2 == 0);
-			}
-
-			Point movementDirection = Navigation.goTo(myLocation, destination, getPassableMap(), getVisibleRobotMap());
-			return move(movementDirection.x, movementDirection.y);
-    	}
-
-    	if (me.unit == SPECS.PILGRIM) {
-    		if (turn == 1) {
-    			log("I am a pilgrim.");
-                 
-                //log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
-    		}
-    	}
-
-    	return null;
-
-	}
+    }
 }
+// package bc19;
+// import java.awt.Point;
+
+// public class MyRobot extends BCAbstractRobot {
+// 	public int turn;
+// 	public Point destination;
+
+//     public Action turn() {
+//     	turn++;
+
+//     	if (me.unit == SPECS.CASTLE) {
+//     		if (turn == 1) {
+//     			log("Building a pilgrim.");
+//     			return buildUnit(SPECS.PILGRIM,1,0);
+//     		}
+
+// 			Robot[] visibleRobots = getVisibleRobots();
+// 			for(Robot r: visibleRobots) {
+// 				if (r.team != me.team) {
+// 					int diffX = r.x - me.x;
+// 					int diffY = r.y - me.y;
+// 					return attack(diffX, diffY);
+// 				}
+// 			}
+
+// 			Point myLocation = new Point(me.x, me.y);
+
+// 			if (destination == null) {
+// 				destination = Navigation.reflect(myLocation, getPassableMap(), me.id % 2 == 0);
+// 			}
+
+// 			Point movementDirection = Navigation.goTo(myLocation, destination, getPassableMap(), getVisibleRobotMap());
+// 			return move(movementDirection.x, movementDirection.y);
+//     	}
+
+//     	if (me.unit == SPECS.PILGRIM) {
+//     		if (turn == 1) {
+//     			log("I am a pilgrim.");
+                 
+//                 //log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
+//     		}
+//     	}
+
+//     	return null;
+
+// 	}
+// }
